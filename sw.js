885194d28919ac356c928b9c974cf9733d60a2f9
Fellow-Soft <joshuacainblackwell@gmail.com>
2026-02-28 23:15:28 -0500

    Initial PWA wrapper for 373 Alexander dashboard

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = '373-alexander-v1';
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).then(function(response) {
+        var clone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, clone);
+        });
+        return response;
+      }).catch(function() {
+        return caches.match(event.request);
+      })
+    );
+    return;
+  }
+  event.respondWith(fetch(event.request));
+});
